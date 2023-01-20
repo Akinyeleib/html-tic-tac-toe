a7c633f3fee104b766b3f8639cc36206309c91dd
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-01-20 12:46:12 +0100

    cell values gotten

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 
-let buttons = document.querySelectorAll("cell");
+let buttons = document.querySelectorAll(".cell");
 
+// alert("Length is: " + buttons.length);
+
+for (var i = 0; i < buttons.length; i++) {
+    // console.log(buttons[i].innerHTML);
+    buttons[i].innerHTML = i;
+}
