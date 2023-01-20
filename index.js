4bf5fbcc07e6365c71738a6db27fbe2bc1498cc7
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-01-20 12:56:50 +0100

    button click change

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
+let isPlayerOneTurn = true;
+
 let buttons = document.querySelectorAll(".cell");
 
-// alert("Length is: " + buttons.length);
-
 for (var i = 0; i < buttons.length; i++) {
-    // console.log(buttons[i].innerHTML);
     buttons[i].innerHTML = i;
+    buttons[i].addEventListener("click", (e) => {
+        e.target.innerHTML = "X";
+    });
 }
+
