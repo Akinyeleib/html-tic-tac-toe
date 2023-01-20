4bfa73eb94275fa5bf5718da2e9793655dee9929
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-01-20 12:59:30 +0100

    double click prevented

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ let isPlayerOneTurn = true;
 let buttons = document.querySelectorAll(".cell");
 
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].innerHTML = i;
+    buttons[i].innerHTML = "";
     buttons[i].addEventListener("click", (e) => {
-        e.target.innerHTML = "X";
+        if (e.target.innerHTML == "") {
+            e.target.innerHTML = isPlayerOneTurn ? "X" : "O";
+            isPlayerOneTurn = !isPlayerOneTurn;
+        } 
     });
 }
 
