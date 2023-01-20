5501ef2c02cb16f77d213088e4e79c69d089d45a
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-01-20 16:00:45 +0100

    color added to cell

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
+// GLobal Variables
+
+let winnings = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+    [0, 4, 8], [2, 4, 7] // diagonal
+];
+
 let isPlayerOneTurn = true;
 
 let buttons = document.querySelectorAll(".cell");
 
 let played = [];
 let text = ""
+
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].innerHTML = "";
@@ -13,7 +22,9 @@ for (var i = 0; i < buttons.length; i++) {
             played = isPlayerOneTurn ? "X" : "O";
             e.target.innerHTML = played;
             isPlayerOneTurn = !isPlayerOneTurn;
+            
         }
     });
 }
+
 
