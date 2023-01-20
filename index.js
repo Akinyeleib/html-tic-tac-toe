0630395e0c9517fc761c8c3648429b6ea4c497ec
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-01-20 13:18:57 +0100

    refactor

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ let isPlayerOneTurn = true;
 
 let buttons = document.querySelectorAll(".cell");
 
+let played = [];
+let text = ""
+
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].innerHTML = "";
     buttons[i].addEventListener("click", (e) => {
         if (e.target.innerHTML == "") {
-            e.target.innerHTML = isPlayerOneTurn ? "X" : "O";
+            played = isPlayerOneTurn ? "X" : "O";
+            e.target.innerHTML = played;
             isPlayerOneTurn = !isPlayerOneTurn;
-        } 
+        }
     });
 }
 
