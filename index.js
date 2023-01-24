03caf3b4d87e4419cfe801e7e386e964c4c3f117
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-01-24 12:41:16 +0100

    played values accessible

--- index.js
+++ index.js
@@ -11,20 +11,32 @@ let isPlayerOneTurn = true;
 
 let buttons = document.querySelectorAll(".cell");
 
-let played = [];
+let played = "";
+let values = [];
 let text = ""
+let playCount = 0;
 
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].innerHTML = "";
     buttons[i].addEventListener("click", (e) => {
         if (e.target.innerHTML == "") {
+            playCount++;
             played = isPlayerOneTurn ? "X" : "O";
+            values[Number(e.target.value)] = played;
             e.target.innerHTML = played;
             isPlayerOneTurn = !isPlayerOneTurn;
-            console.log(e.target.value);
+            console.log(values);
+            // console.log(e.target.value);
+            if (playCount == 9) 
+                loadValues();
         }
     });
 }
 
-
+function loadValues () {
+    console.log('hello');
+    for (var i = 0; i < buttons.length; i++) {
+        console.log(buttons[i].innerHTML);
+    }
+}
